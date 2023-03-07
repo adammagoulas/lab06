f970255c7466302dff14bbbc02e6b411c7a1d3c5
shankaplier <guttashashank@gmail.com>
2023-03-07 10:33:47 -0500

    Update lab06.py
    Added decoder function

--- lab06.py
+++ lab06.py
@@ -13,8 +13,27 @@ def encoder(code, encryption):
         encrypted_code_list += str(i)
     return (encrypted_code_list)
 
+#This is done by G Shashank
+def decoder(code, numbertoberemoved):
+    decodedstring = ""
+    code = str(code)
+    for i in code:
+        decodednumber = int(i) - numbertoberemoved
+        if decodednumber < 0:
+            finalnumber = 10 + decodednumber
+            decodedstring += str(finalnumber)
+        else:
+            decodedstring += str(decodednumber)
+
+    return decodedstring
+
+
+
 if __name__ == "__main__":
+    '''
     #This takes in a user input
     enc = int(input("What value would you like to use to encrypt your password: "))
-    print(encoder(1234555, enc))
+    print(encoder(1234555, enc))'''
 
+    y = decoder(45678888, 3)
+    print(y)
