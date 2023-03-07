b53b83fbecd2c527e3ab7271caa49d7e7026bd64
adammagoulas <127216796+adammagoulas@users.noreply.github.com>
2023-03-07 10:00:43 -0500

    Add files via upload
    This file has the encoding function for lab06

---
+++ lab06.py
@@ -0,0 +1,17 @@
+def encoder(code, encryption):
+    code = str(code)
+    code_list = list(code)
+    index = 0
+    for char in code_list:
+        char = int(char)
+        char += encryption
+        code_list[index] = char
+        index+=1
+    encrypted_code_list = ''
+    for i in code_list:
+        encrypted_code_list += str(i)
+    return (encrypted_code_list)
+
+if __name__ == "__main__":
+    enc = int(input("What value would you like to use to encrypt your password: "))
+    print(encoder(1234555, enc))
